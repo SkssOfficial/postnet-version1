979226fa11139142b59c7b69e1c3739abeab192c
C&MASKSS <78840918+SkssOfficial@users.noreply.github.com>
2021-03-25 12:05:19 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -202,11 +202,10 @@ function countMovement() {
                                 
                 break;
             case '2':
-                   if (leftKnee.y > leftElbow.y + poseOffset && leftKnee.confidence > confidenceOffset && leftElbow.confidence > confidenceOffset) {
+                if (leftKnee.y > leftElbow.y + poseOffset + 5 && leftKnee.y > rightKnee.y + poseOffset && leftKnee.confidence > confidenceOffset && leftElbow.confidence > confidenceOffset) {
                     stageResult[0] += 1
-                } else if (rightKnee.y > rightElbow.y + poseOffset && rightKnee.confidence > confidenceOffset && rightElbow.confidence > confidenceOffset) {
+                } else if (rightKnee.y > rightElbow.y + poseOffset + 5 && rightKnee.y > leftKnee.y + poseOffset && rightKnee.confidence > confidenceOffset && rightElbow.confidence > confidenceOffset) {
                     stageResult[1] += 1
-                }
                 break;
             default:
                 break;
